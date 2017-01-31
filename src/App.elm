bc17038c3a6802ac3172ae14c882a5d28f0e5b17
Josh Greenwood <joshua.t.greenwood@gmail.com>
2017-01-30 20:40:09 -0500

    adds webhook listener on elm side

--- src/App.elm
+++ src/App.elm
@@ -1,37 +1,69 @@
 module App exposing (..)
 
-import Html exposing (Html, text, div, img)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Events exposing (onClick)
+import WebSocket exposing (listen, send)
 
 
 type alias Model =
-    { message : String
-    , logo : String
+    { streamTime : Bool
+    , time : String
     }
 
 
 init : String -> ( Model, Cmd Msg )
 init path =
-    ( { message = "Your Elm App is working!", logo = path }, Cmd.none )
+    ( { streamTime = False, time = "" }, Cmd.none )
 
 
 type Msg
-    = NoOp
+    = ToggleStreaming
+    | Time String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        ToggleStreaming ->
+            let
+                newModel =
+                    { model | streamTime = not model.streamTime }
+
+                msg =
+                    if newModel.streamTime then
+                        "start"
+                    else
+                        "stop"
+                cmd =
+                    send websocketURL msg
+            in
+                ( newModel , cmd )
+
+        Time newTime ->
+            ( { model | time = newTime }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src model.logo ] []
-        , div [] [ text model.message ]
-        ]
+    let
+        toggleLabel =
+            if model.streamTime then
+                "Stop"
+            else
+                "Start"
+    in
+        div []
+            [ button [ onClick ToggleStreaming ] [ text toggleLabel ]
+            , br [] []
+            , text model.time
+            ]
+
+
+websocketURL : String
+websocketURL =
+    "ws://localhost:3028"
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    listen websocketURL Time
